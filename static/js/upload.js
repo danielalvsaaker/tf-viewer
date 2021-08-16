9e89563e0de5e2b9ee16ec89661882263f47eeda
vidarandrebo <va.bo@stud.uis.no>
2021-08-16 20:00:58 +0200

    initial upload test

--- static/js/upload.js
+++ static/js/upload.js
@@ -1,7 +1,36 @@
 const Upload = {
     props: ["user"],
     template: `
-        <p>Welcome to the upload site</p>
+            <div class="new">
+                <form id="newpost" method="POST" enctype=mulitpart/form-data v-on:submit="publishPost">
+                    <h4>Upload</h4>
+                    <label for="fileupload">Upload </label>
+                    <p v-if="error">{{ error }}</p>
+                    <input type="file" ref="uploadedFiles" name="fileupload" multiple>
+                    <input type="submit" value="Upload">
+                </form>
+            </div>
     `,
-
+    data: function(){
+        return {
+            files: [],
+            error: null
+        }
+    },
+    methods:{
+        publishPost: async function() {
+            event.preventDefault();
+            this.error = null;
+            this.files = this.$refs.uploadedFiles.files;
+            let formData = new FormData();
+            formData.append("activity",this.files[0], this.files[0].name);
+            let response = await fetch("/user/asd/activity", {
+                body: formData,
+                method: "POST",
+            });
+            let result = await response;
+            console.log(response);
+            this.$refs.uploadedFiles.value = null;
+        }
+    }
 }
